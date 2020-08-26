49916a6b0a861eba2f030cbb49aa3ce920a2e56d
Tess Sb <stesnime@yahoo.com>
2020-08-26 15:21:45 +0200

    second commit

---
+++ Cartes-RebutsSolution/Cartes-Rebuts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cartes_Rebuts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
